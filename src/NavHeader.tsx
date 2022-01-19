9c75f50ade3b7a5254b7d75473af3b3edb56c4a2
Ryan Tate <ryantate@Joshuas-MBP.attlocal.net>
2022-01-19 16:32:15 -0600

    changed url

--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -2,13 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 
 export interface ProjectPage{
     ProjectName: string
@@ -17,17 +13,12 @@ export interface ProjectPage{
 
 const projectPages: ProjectPage [] = [
     {ProjectName: 'Movie Trivia',
-    ProjectSite: 'https://www.movietrivia.ryantatecodes.com/'}
+    ProjectSite: 'https://movietrivia.ryantatecodes.com/'}
 ]
 
-const pages = ['Movie Trivia'];
 
 export const NavHeader: React.FC = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  
 
 
   return (
